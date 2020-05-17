94a1db36e46d6ec5403d44c979fa5ebac2f3fece
Kislay Singh <43761382+SinghKislay@users.noreply.github.com>
2020-05-17 23:14:11 +0530

    improvements

--- inference.py
+++ inference.py
@@ -12,32 +12,26 @@ test_model.use_release()
 
 #predict image
 pre_process_image_in_folder("./data", "./processed")
-'''
+
 images = os.listdir('./processed')
 for image in images:
-    #image_path = os.path.join("./processed", image)
-    image_path = get_data("2019_YELL_2_541000_4977000_image_crop.tiff")
-    img = test_model.predict_tile(image_path, return_plot = True, patch_size=300,patch_overlap=0.005)
+    image_path = os.path.join("./processed", image)
+    raster = cv2.imread(image_path)
+    numpy_image = np.array(raster)
+    windows = preprocess.compute_windows(numpy_image, patch_size=400,patch_overlap=0.1)
+    for window in windows:
+        crop = numpy_image[window.indices()]
+
+        img = test_model.predict_image(numpy_image = crop,return_plot=True, score_threshold=0.05)
 
 
-#Show image, matplotlib expects RGB channel order, but keras-retinanet predicts in BGR
-    plt.imshow(img[...,::-1])
-    plt.title(img)
-    plt.show()
+        #Show image, matplotlib expects RGB channel order, but keras-retinanet predicts in BGR
+        plt.imshow(img[...,::-1])
+        plt.show()
+    
 #raster = cv2.imread("./processed/IITK1.JPG")
 #numpy_image = np.array(raster)
 #windows = preprocess.compute_windows(numpy_image, patch_size=400,patch_overlap=0.1)
 #image = model.predict_image(numpy_image = crop,return_plot=True, score_threshold=0.05)
-'''
-raster = cv2.imread("./processed/IITK.JPG")
-numpy_image = np.array(raster)
-windows = preprocess.compute_windows(numpy_image, patch_size=400,patch_overlap=0.1)
-for window in windows:
-    crop = numpy_image[window.indices()]
-
-    img = test_model.predict_image(numpy_image = crop,return_plot=True, score_threshold=0.05)
 
 
-    #Show image, matplotlib expects RGB channel order, but keras-retinanet predicts in BGR
-    plt.imshow(img[...,::-1])
-    plt.show()
